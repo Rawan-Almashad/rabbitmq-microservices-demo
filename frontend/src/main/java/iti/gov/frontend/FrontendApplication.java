e0074bfd99707ce524b20f78308e3e588ba3c8fd
Rawan <rawanalmashad@gmail.com>
2026-06-08 16:04:21 +0300

    Add RestTemplate

--- frontend/src/main/java/iti/gov/frontend/FrontendApplication.java
+++ frontend/src/main/java/iti/gov/frontend/FrontendApplication.java
@@ -2,12 +2,18 @@ package iti.gov.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FrontendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrontendApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
