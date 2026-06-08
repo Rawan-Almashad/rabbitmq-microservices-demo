5887a7bf1f75688606147c4d2a1a501622f165ed
Rawan <rawanalmashad@gmail.com>
2026-06-08 15:51:24 +0300

    edit RegistrationController to so the frontend service send him json

--- producer/src/main/java/iti/gov/producer/controller/RegistrationController.java
+++ producer/src/main/java/iti/gov/producer/controller/RegistrationController.java
@@ -5,61 +5,47 @@ import iti.gov.producer.entity.User;
 import iti.gov.producer.service.UserService;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
-@Controller
+@RestController
+@RequestMapping("/producer")
 public class RegistrationController {
 
     private UserService userService;
     private RabbitTemplate rabbitTemplate;
 
     @Autowired
-    public RegistrationController(UserService userService , RabbitTemplate rabbitTemplate)
-    {
+    public RegistrationController(UserService userService,
+                                  RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
         this.userService = userService;
     }
-    @GetMapping("/")
-    public String showRegistrationForm(Model model) {
-        model.addAttribute("user", new User());
-        return "register";
-    }
-
-    @GetMapping("/users")
-    public String showAllUsers(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
-        return "users";
-    }
 
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute User user, Model model) {
-        try {
-            User savedUser = userService.registerUser(user);
+    public ResponseEntity<String> registerUser(
+            @RequestBody User user) {
 
-            UserRegisteredEvent message = new UserRegisteredEvent(
-                    (long) savedUser.getId(),
-                    savedUser.getFirstname(),
-                    savedUser.getLastname(),
-                    savedUser.getUsername(),
-                    LocalDateTime.now()
-            );
+        User savedUser = userService.registerUser(user);
 
-            rabbitTemplate.convertAndSend("user-registration", message);
+        UserRegisteredEvent message =
+                new UserRegisteredEvent(
+                        (long) savedUser.getId(),
+                        savedUser.getFirstname(),
+                        savedUser.getLastname(),
+                        savedUser.getUsername(),
+                        LocalDateTime.now()
+                );
 
-            model.addAttribute("success", true);
-            model.addAttribute("message", "User registered successfully!");
+        rabbitTemplate.convertAndSend(
+                "user-registration",
+                message);
 
-        } catch (Exception e) {
-            model.addAttribute("error", e.getMessage());
-        }
-
-        model.addAttribute("user", new User());
-        return "register";
+        return ResponseEntity.ok(
+                "User registered successfully!");
     }
 }
