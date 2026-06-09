556c0b3a9753ba73f196dd0bb886831da03d1b5d
Rawan <rawanalmashad@gmail.com>
2026-06-09 19:20:57 +0300

    Adding EurekaServer and APIGateWay

---
+++ eureka-server/src/test/java/iti/gov/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package iti.gov.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
