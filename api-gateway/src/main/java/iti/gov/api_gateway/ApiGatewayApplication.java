8f7159f7d8c1fec7afbd16c46cbb6032d60719e3
Rawan <rawanalmashad@gmail.com>
2026-06-10 04:08:28 +0300

    Add RouteLocator to add routes

--- api-gateway/src/main/java/iti/gov/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/iti/gov/api_gateway/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package iti.gov.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -9,5 +12,18 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("producer", r -> r
+						.path("/producer/**")
+						.uri("lb://producer"))
+				.route("consumer", r -> r
+						.path("/consumer/**")
+						.uri("lb://consumer"))
+				.route("frontend", r -> r
+						.path("/frontend/**")
+						.uri("lb://frontend"))
+				.build();
+	}
 }
